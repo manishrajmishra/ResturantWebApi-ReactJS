f6c70ac4e949fb78c5482e0a68f640473f0520c3
Manish Raj <Manish.Raj@programmers.io>
2021-04-01 18:07:10 +0530

    Add project files.

---
+++ ResturantWebApi+ReactJS/Models/Customer.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace ResturantWebApi_ReactJS.Models
+{
+    public class Customer
+    {
+        [Key]
+        public int CustomerId { get; set; }
+
+        [Column(TypeName = "nvarchar(100)")]
+        public string CustomerName { get; set; }
+    }
+}
---
+++ ResturantWebApi+ReactJS/Models/ResturantDbContext.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ResturantWebApi_ReactJS.Models
+{
+    public class ResturantDbContext : DbContext
+    {
+        public ResturantDbContext(DbContextOptions<ResturantDbContext> options) : base(options)
+        {
+
+        }
+
+        public DbSet<Customer> Customers { get; set; }
+        public DbSet<FoodItem> FoodItems { get; set; }
+        public DbSet<OrderMaster> OrderMasters { get; set; }
+        public DbSet<OrderDetail> OrderDetails { get; set; }
+    }
+}
